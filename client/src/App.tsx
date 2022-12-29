118503b161324fe35f8a795fc69c113c7e23b132
leandro <fahurleandro@gmail.com>
2022-12-29 13:40:48 -0800

    configured storybook and vite with styled components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,11 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  font-weight: bold;
+  color: red;
+`;
+
 export const App = () => {
-  return <div>App Component</div>;
+  return <Title>App Component</Title>;
 };
